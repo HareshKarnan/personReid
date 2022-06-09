f987a68c3e4c5e4bb0cf89c1c23c4137eee7564e
Haresh Karnan <haresh.miriyala@gmail.com>
2022-06-09 17:47:03 -0500

    not using pytorch lightning during inference

--- run_inference.py
+++ run_inference.py
@@ -3,8 +3,24 @@ import pytorch_lightning as pl
 import cv2
 import torch
 import numpy as np
+import torch.nn as nn
 
 device = torch.device("cuda")
+
+model = nn.Sequential(
+			nn.Conv2d(3, 16, kernel_size=3, stride=2, bias=False),  # 63 x 63
+			nn.BatchNorm2d(16), nn.ReLU(),
+			nn.Conv2d(16, 32, kernel_size=3, stride=2, bias=False),  # 31 x 31
+			nn.BatchNorm2d(32), nn.ReLU(),
+			nn.Conv2d(32, 64, kernel_size=5, stride=2, bias=False),  # 14 x 14
+			nn.BatchNorm2d(64), nn.ReLU(),
+			nn.Conv2d(64, 128, kernel_size=5, stride=2, bias=False),  # 5 x 5
+			nn.BatchNorm2d(128), nn.ReLU(),
+			nn.Conv2d(128, 256, kernel_size=3, stride=2),  # 2 x 2
+			nn.ReLU(),
+			nn.Flatten(),  # 1024 output
+			nn.Linear(1024, 128)
+		)
 
 def cv2_img_to_tensor(cv2_img):
 	"""
@@ -23,7 +39,10 @@ def cv2_img_to_tensor(cv2_img):
 	return img
 
 # load the pytorch lightning model from checkpoint on CUDA
-model = BarlowModel(latent_size=64).load_from_checkpoint('models/07-06-2022-12-49-07_.ckpt', latent_size=128).to(device=device)
+# model = BarlowModel(latent_size=64).load_from_checkpoint('models/07-06-2022-12-49-07_.ckpt', latent_size=128).to(device=device)
+
+model.load_state_dict({k.replace('visual_encoder.',''):v for k,v in torch.load('models/07-06-2022-12-49-07_.ckpt')['state_dict'].items()})
+model.to(device=device)
 
 # read the image from disk [you might have other ways to get this image of a person - from detectron/yolo/etc]
 img = cv2.imread('test/test.png', cv2.IMREAD_COLOR)
@@ -32,6 +51,7 @@ img = cv2.imread('test/test.png', cv2.IMREAD_COLOR)
 tensor_img = cv2_img_to_tensor(img)
 
 # run inference
-descriptor = model.visual_encoder(tensor_img.to(device=device))
+descriptor = model(tensor_img.to(device=device))
+# descriptor = model.visual_encoder(tensor_img.to(device=device))
 
 print(descriptor.detach().cpu().numpy())
